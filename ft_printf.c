e7ba759746737fe0685d9d28833a8a1e782ee54d
Camilo Andres Garatejo Moreno <cgaratej@cbr6s1.42barcelona.com>
2024-02-02 18:00:42 +0100

    add project

---
+++ ft_printf.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgaratej <cgaratej@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 10:38:13 by cgaratej          #+#    #+#             */
+/*   Updated: 2024/02/02 16:27:03 by cgaratej         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	ft_arg_type(char c, va_list element)
+{
+	if (c == 'c')
+		return (ft_putchar(va_arg(element, int)));
+	if (c == 's')
+		return (ft_putstr(va_arg(element, char *)));
+	if (c == 'p')
+		return (0);
+	if (c == 'd' || c == 'i')
+		return (ft_putnbr(va_arg(element, int)));
+	if (c == 'u')
+		return (ft_putnbr_nosig(va_arg(element, unsigned int)));
+	if (c == 'x')
+		return (0);
+	if (c == 'X')
+		return (0);
+	return (0);
+}
+
+static int	ft_ispercentage(char c, va_list element)
+{
+	int	len;
+
+	if (c != '%')
+	{
+		len = ft_arg_type(c, element);
+		if (len == -1)
+			return (-1);
+		return (len);
+	}
+	else
+	{
+		if (ft_putchar(c) == -1)
+			return (-1);
+	}
+	return (len);
+}
+
+int	ft_printf(char const *str, ...)
+{
+	va_list	args;
+	int		i;
+	int		len;
+
+	i = 0;
+	va_start(args, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			len = ft_ispercentage(str[i + 1], args);
+			if (len == -1)
+				return (-1);
+			i++;
+		}
+		else
+		{
+			if (ft_putchar(str[i]) == -1)
+				return (-1);
+			len++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (len);
+}
---
+++ ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgaratej <cgaratej@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 10:35:39 by cgaratej          #+#    #+#             */
+/*   Updated: 2024/02/02 16:31:12 by cgaratej         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int	ft_printf(char const *str, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putnbr(int n);
+int	ft_putnbr_nosig(unsigned int n);
+
+#endif
